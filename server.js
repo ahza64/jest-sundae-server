7081153b280bad67b00161934ae2c6bc396fdeb5
ahza64 <justinmichaelpettit@gmail.com>
2022-05-28 13:56:35 -0700

    cors bug

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const fs = require('fs');
 const app = express();
 
 // CORS for react app, assuming port 3000
-// app.use(cors({
-//   origin: 'http://localhost:3000',
-//   credentials: true
-// }))
+app.use(cors({
+  origin: 'https://jest-sundae.herokuapp.com/',
+  credentials: true
+}))
 
 // use middleware to serve static images
 app.use(express.static('public'))
